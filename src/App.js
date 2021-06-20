98d0fb16392cb5822f0e19d9d9ed924f25a66300
vasanth_nayak <vasanth@vhitechlab.com>
2021-06-20 16:46:02 +0530

    Vasanth | Mapping test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Viewer,
+  Entity,
+  PointGraphics,
+  EntityDescription,
+  CameraFlyTo,
+} from "resium";
+import { Cartesian3 } from "cesium";
+import { Container, Dropdown } from "react-bootstrap";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+} from "@material-ui/pickers";
+import tzlookup from "tz-lookup";
+import DateFnsUtils from "@date-io/date-fns";
+import { citiesList } from "./Constants/CityLists";
 
-function App() {
+
+const App = () => {
+  const [city, setCity] = useState(null);
+
+  const handleChange = (cityName, long, lat) => {
+    const timeZone = tzlookup(lat, long);
+    let date = new Date(new Date().toLocaleString("en-US", { timeZone }));
+    setCity({ city: cityName, long, lat, time: date });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Container>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            height: 30,
+            margin: "20px 0px 20px 0px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              {city ? city?.city : "Cities"}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              {citiesList.map((city) => (
+                <Dropdown.Item
+                  onSelect={() => {
+                    handleChange(city.name, city.longitude, city.latitude);
+                  }}
+                  key={city.id}
+                >
+                  {city.name}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+
+          {city?.time && (
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardTimePicker value={city?.time} readOnly />
+            </MuiPickersUtilsProvider>
+          )}
+        </div>
+        <Viewer>
+          {city && (
+            <div>
+              <CameraFlyTo
+                duration={5}
+                destination={Cartesian3.fromDegrees(city.long, city.lat, 10000)}
+              />
+              <Entity
+                position={Cartesian3.fromDegrees(city.long, city.lat, 10000)}
+                name={city.city}
+              >
+                <PointGraphics pixelSize={10} />
+                <EntityDescription>
+                  <h1>{`This is ${city.city}`}</h1>
+                </EntityDescription>
+              </Entity>
+            </div>
+          )}
+        </Viewer>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Constants/CityLists.js
@@ -0,0 +1,21 @@
+export const citiesList = [
+    {
+      id: 0,
+      name: "London",
+      longitude: -0.1278,
+      latitude: 51.5074,
+    },
+    {
+      id: 1,
+      name: "Paris",
+      longitude: 2.3522,
+      latitude: 48.8566,
+    },
+    {
+      id: 2,
+      name: "Frankfurt",
+      longitude: 8.6821,
+      latitude: 50.1109,
+    },
+  ];
+  
